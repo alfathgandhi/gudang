91cb59e4e9de3095578f76cf70d506933be4d023
alfathgandhi <gandhialfath98@gmail.com>
2019-11-08 23:24:43 +0700

    pertama

---
+++ Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/koneksi.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ *
+ * @author wibudesu
+ */
+public class koneksi {
+    private static Connection mysqlconfig;
+    
+    public static Connection configDB()throws SQLException{
+        try {
+            String url="jdbc:mysql://localhost/gudang"; //url database
+            String user="root"; //user database
+            String pass=""; //password database
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlconfig=DriverManager.getConnection(url, user, pass);            
+        } catch (Exception e) {
+            System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+        }
+        return mysqlconfig;
+    }    
+       
+
+}
